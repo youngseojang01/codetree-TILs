fc512a5e2eac3aeec3adf2a74aed5e72e8c2c0e1
youngseojang01 <83485151+youngseojang01@users.noreply.github.com>
2023-11-20 21:32:10 +0900

    Title: 황금비율 토스트, Time: 7ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 231120/황금비율 토스트/golden-toast.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    list<char> l;
+    char c;
+    for (int i=0; i<n; i++){
+        cin >> c;
+        l.push_back(c);
+    }
+
+    list<char>::iterator it = l.end();
+    char recipe;
+    char s;
+    for(int i=0; i<m; i++) {
+        cin >> recipe;
+        if (recipe == 'L'){
+            if (it==l.begin()) continue;
+            it--;
+        }
+        else if (recipe == 'R'){
+            if (it==l.end()) continue;
+            it++;
+        }
+        else if (recipe == 'D'){
+            it=l.erase(it);
+        }
+        else if (recipe == 'P'){
+            cin >> s;
+            l.insert(it, s);
+        }
+    }
+
+    for(list<char>::iterator iter=l.begin(); iter!=l.end(); iter++){
+        cout << *iter;
+    }
+    return 0;
+}
