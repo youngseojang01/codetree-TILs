30258e87018ec14519c64825f1ad9e2652d4a913
youngseojang01 <83485151+youngseojang01@users.noreply.github.com>
2023-11-21 13:03:22 +0900

    Title: 거품 정렬 구현, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231121/거품 정렬 구현/implement-bubble-sort.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    int arr[100];
+    for (int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+
+    bool sorted = false;
+    while(!sorted){
+        sorted = true;
+        for (int i=0; i<n; i++){
+            if (arr[i]>arr[i+1]){
+                int tmp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = tmp;
+                sorted = false;
+            }
+        }
+    }
+
+    for (int i=0; i<n; i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
