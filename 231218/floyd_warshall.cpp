e630652965938fd3e90c604d60413d970c660785
youngseojang01 <83485151+youngseojang01@users.noreply.github.com>
2023-12-18 17:46:08 +0900

    Create floyd_warshall.cpp

---
+++ 231218/floyd_warshall.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+#define MAX 100000
+
+int main() {
+  int graph[6][6];
+  int w[6];
+
+  cout << "input: \n";
+  for (int i=0; i<6; i++) {
+    for (int j = 0; j < 6; j++) {
+      if (i == j)
+        graph[i][j] = 0;
+      else 
+        graph[i][j] = MAX;
+      int num;
+      cin >> num;
+      if (num != 0) graph[i][j] = num;
+    }
+  }
+
+
+  for (int i = 0; i < 6; i++) {
+    for (int j = 0; j < 6; j++) {
+      for (int k = 0; k < 6; k++) {
+        if (graph[j][k] > graph[j][i] + graph[i][k]) {
+          graph[j][k] = graph[j][i] + graph[i][k];
+        }
+      }
+    }
+  }
+
+  cout << "output: \n";
+  for (int i = 0; i < 6; i++) {
+    for (int j = 0; j < 6; j++) {
+      cout << graph[i][j] << " ";
+    }
+    cout << "\n";
+  }
+  return 0;
+}
